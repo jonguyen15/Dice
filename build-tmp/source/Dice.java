f71c8a668f514b5b40c107a9c11d9767d04db8cb
unknown <kijensen@697-334-Faramir.sf.k12.internal>
2014-09-11 11:36:03 -0700

    Dice.html

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,14 +1,30 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 Die neo=new Die(50, 125);
 Die mrFudge=new Die(200,125);
 
-void setup()
+public void setup()
 {
     noLoop();
     size(300,300);
     textMode(CENTER);
     textSize(24);
 }
-void draw()
+public void draw()
 {
     background(255);
     int sum=neo.dots+mrFudge.dots;
@@ -20,7 +36,7 @@ void draw()
     mrFudge.roll();
 }
 
-void mousePressed()
+public void mousePressed()
 {
     redraw();
 }
@@ -32,11 +48,11 @@ class Die //models one single dice cube
         myX=x;
         myY=y;
     }
-    void roll()
+    public void roll()
     {
         dots=(int)(Math.random()*6+1);
     }
-    void show()
+    public void show()
     {
         fill(125);
         rect(myX,myY,50,50,10);
@@ -87,4 +103,13 @@ class Die //models one single dice cube
             System.out.println("dots: "+dots);
         }    
     }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
